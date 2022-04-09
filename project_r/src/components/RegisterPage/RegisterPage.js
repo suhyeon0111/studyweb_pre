c4b9d9c9768f64e1739f5deba099c502446afd4b
suhyeon0111 <soo4833@naver.com>
2022-04-09 23:46:55 +0900

    수정 register페이지에 아이콘 추가

--- project_r/src/components/RegisterPage/RegisterPage.js
+++ project_r/src/components/RegisterPage/RegisterPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { FaLock } from 'react-icons/fa';
 import './RegisterPage.css';
 
 const RegisterPage = () => {
@@ -6,9 +8,15 @@ const RegisterPage = () => {
     <div className="LoginTemplate">
       <div className="ERUM">ERUM</div>
       <div>
-        <div className="LoginId"></div>
-        <div className="LoginPassword_1"></div>
-        <div className="LoginPassword_2"></div>
+        <div className="LoginId">
+          <BsFillPersonFill className="icon-ID" size="33" color="#17a934" />
+        </div>
+        <div className="LoginPassword_1">
+          <FaLock className="icon-Password" size="33" color="#17a934" />
+        </div>
+        <div className="LoginPassword_2">
+          <FaLock className="icon-Password" size="33" color="#17a934" />
+        </div>
         <div className="LoginButton">LOGIN</div>
       </div>
     </div>
