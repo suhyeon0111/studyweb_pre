adb60e9dfda5281cbb81be5d165aa496fad91990
suhyeon <soo4833@naver.com>
2022-04-30 23:14:55 +0900

    [test]

--- project_r/src/components/MainPage/MainPage.js
+++ project_r/src/components/MainPage/MainPage.js
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import TimePage from '../TimePage/TimePage';
 import './MainPage.css';
+
+import 'react-router-dom';
+
+
 
 function MainPage() {
   const [date, setDate] = useState(new Date());
 
+  const events = () => {
+    document.location.href('/addtime')
+  }
   return (
     <div className='app'>
       <div className='text-center'><span>ERUM</span></div>
-        <Calendar onChange={setDate} value={date} />
+        <Calendar onChange={setDate} value={date} 
+          onClickDay = {events}
+        />
     </div>
   );
 }
